cf0d2fd32538591f5b961f27cbb3138e330830bc
MinhQuangPtit <vude100@gmail.com>
2021-10-19 21:28:20 +0700

    Deploy project

---
+++ js/myJs.js
@@ -0,0 +1,156 @@
+const textConfig = {
+  text1: "He luu em! Là anh Quang đây :3",
+  text2: "Hôm nay là ngày đặc biệt, anh có điều bất ngờ dành cho em",
+  text3: "Hôm nay là ngày 20/10, em muốn nhận quà gì nào ?",
+  text4: " ",
+  text5: " Món quà vật chất siu to :D",
+  text6: " Món quà tinh thần :)))",
+  text7: "Em thích quà tinh thần gì nào ? Nhập vào đây và quà sẽ gửi tới em ngay lập tức : )",
+  text8: "Nhận quà",
+  text9: "Lời chúc của anh chính là món quà ý nghĩa nhất <3333333333333333333333333",
+  text10: " ",
+  text11:
+    " 20/10 chúc cho em luôn xinh đẹp, may mắn và sớm có người yêu :) ",
+  text12: "Gửi lời cảm ơn tới anh <3",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+
+  $("#text3").html(textConfig.text3);
+  $("#text4").html(textConfig.text4);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  function firstQuestion() {
+    $(".content").hide();
+    Swal.fire({
+      title: textConfig.text1,
+      text: textConfig.text2,
+      imageUrl: "img/cuteCat.jpg",
+      imageWidth: 300,
+      imageHeight: 300,
+      background: '#fff url("img/iput-bg.jpg")',
+      imageAlt: "Custom image",
+    }).then(function () {
+      $(".content").show(200);
+    });
+  }
+
+  // switch button position
+  function switchButton() {
+    var audio = new Audio("sound/duck.mp3");
+    audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button póition
+  function moveButton() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n <= 2) switchButton();
+    if (n > 3) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+  // generate text in input
+  function textGenerate() {
+    var n = "";
+    var text = " " + textConfig.text9;
+    var a = Array.from(text);
+    var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+      for (let i = 1; i <= count; i++) {
+        n = n + a[i];
+        if (i == text.length + 1) {
+          $("#txtReason").val("");
+          n = "";
+          break;
+        }
+      }
+    }
+    $("#txtReason").val(n);
+  }
+
+  // show popup
+  $("#yes").click(function () {
+    var audio = new Audio("sound/tick.mp3");
+    audio.play();
+    Swal.fire({
+      title: textConfig.text7,
+      html: true,
+      width: 900,
+      padding: "3em",
+      html: "<input type='text' class='form-control' id='txtReason'  placeholder='Whyyy'>",
+      background: '#fff url("img/iput-bg.jpg")',
+      backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("img/giphy2.gif")
+                    left top
+                    no-repeat
+                  `,
+      showCancelButton: false,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonColor: "#fe8a71",
+      cancelButtonColor: "#f6cd61",
+      confirmButtonText: textConfig.text8,
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          width: 900,
+          confirmButtonText: textConfig.text12,
+          background: '#fff url("img/iput-bg.jpg")',
+          title: textConfig.text10,
+          text: textConfig.text11,
+          confirmButtonColor: "#83d0c9",
+          onClose: () => {
+            window.location = "http://fb.com/quangdt102";
+          },
+        });
+      }
+    });
+
+    $("#txtReason").focus(function () {
+      var handleWriteText = setInterval(function () {
+        textGenerate();
+      }, 10);
+      $("#txtReason").blur(function () {
+        clearInterval(handleWriteText);
+      });
+    });
+  });
+});
